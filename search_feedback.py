aac8485f654e4d75745a3e61b34da205f7d896f6
sagarabhinand <sagarabhinand342@gmail.com>
2025-04-09 12:44:13 +0530

    Update search_feedback.py

--- search_feedback.py
+++ search_feedback.py
@@ -1,1 +1,5 @@
+def search_feedback(feedback_list, keyword):
+    """Search feedback entries by a keyword in the comments."""
+    results = [feedback for feedback in feedback_list if keyword.lower() in feedback.get("comment", "").lower()]
+    return results
 
